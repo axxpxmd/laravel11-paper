b4e427712b9332c9d670944f05864523e1d9892d
Asip Hamdi <asiphamdi@gmail.com>
2025-01-16 11:03:24 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,103 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <title>Login - SPIP</title>
+    <link href="{{ asset('images/template/tangsel.png') }}" rel="icon" type="image/png">
 
-        <!-- Username -->
-        <div>
-            <x-input-label for="username" :value="__('Email')" />
-            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('username')" class="mt-2" />
+    <!-- LINEARICONS -->
+    <link rel="stylesheet" href="{{ asset('assetLogin/fonts/linearicons/style.css') }}">
+
+    <!-- STYLE CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assetLogin/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assetLogin/css/util.css') }}">
+
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+</head>
+<body style="font-family: 'Poppins', sans-serif">
+    <div class="wrapper">
+        <div class="inner">
+            <img src="{{ asset('assetLogin/images/survey.png') }}" alt="" class="image-1" width="300px" style="margin-left: -80px !important; margin-bottom: 24px !important">
+            <div class="text-center">
+                <div class="row">
+                    <div class="col-4 p-0">
+                        <img src="{{ asset('images/template/tangsel.png') }}" class="img-fluid mb-2" width="110" alt="">
+                    </div>
+                    <div class="col-8 p-0 text-left fs-25 text-white" style="font-weight: bolder">
+                        <p>LARAVEL PAPER</p>
+                        <p>LOREM IPSUM DOLER</p>
+                    </div>
+                </div>
+            </div>
+            <form method="POST" action="{{ route('login') }}" class="needs-validation" style="border-radius: 20px" novalidate>
+                @csrf
+                <div class="text-center" style="margin-top: -40px !important;">
+                    <p class="fw-bold fs-25 text-black">LARAVEL PAPER</p>
+                    <p class="text-black fs-14 mt-1">Template paper + Laravel 11</p>
+                </div>
+                <div class="mt-4">
+                    <div class="form-holder">
+                        <span class="lnr lnr-user text-black-50"></span>
+                        <input type="text" class="form-control-l" name="username" autocomplete="username" placeholder="Masukan Username" value="{{ old('username') }}" autofocus required>
+                        <div class="invalid-feedback fw-bold fs-10 mt-2 mb-3" style="position: absolute;">
+                            Username wajib diisi.
+                        </div>
+                    </div>
+                    <div class="form-holder mt-4">
+                        <span class="lnr lnr-lock text-black-50"></span>
+                        <input type="password" class="form-control-l" name="password" placeholder="Masukan Password" required>
+                        <div class="invalid-feedback fs-10 fw-bold mt-2" style="position: absolute;">
+                            Password wajib diisi.
+                        </div>
+                    </div>
+                    @if (session('error'))
+                    <div class="alert alert-danger fs-14 text-center fw-bold" style="margin-bottom: -20px !important; margin-top: 35px !important" role="alert">
+                        Username / Password Salah.
+                    </div>
+                    @endif
+                    @if (count($errors) > 0)
+                    <div class="alert alert-danger fs-12 text-center" style="margin-bottom: -25px !important" role="alert">
+                        Username / Password salah!
+                    </div>
+                    @endif
+                    <button type="submit" class="rounded">MASUK</button>
+                </div>
+            </form>
+            <p class="text-center text-white fw-bold fs-14 mt-2">LARAVEL PAPER © {{ date('Y') }}</p>
+            <img src="{{ asset('assetLogin/images/image3.png') }}" alt="" class="image-2" style="margin-right: -100px !important; margin-bottom: -5px !important" width="300">
         </div>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <script src="{{ asset('assetLogin/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('assetLogin/js/main.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (() => {
+        'use strict'
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+            form.classList.add('was-validated')
+            }, false)
+        })
+        })()
+    </script>
+</body>
+</html>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,100 +1,122 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
+@php
+    $totalRevisi = App\TmResult::select('tm_results.id', 'tm_quesioners.tahun_id')
+                    ->join('tm_quesioners', 'tm_quesioners.id', '=', 'tm_results.quesioner_id')
+                    ->where('tm_results.user_id', Auth::user()->id)
+                    ->where('status_revisi', 1)
+                    ->count();
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-            </div>
-
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </div>
-    </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
-        </div>
-    </div>
-</nav>
+    $role = Auth::user()->modelHasRole->role_id;
+    $getUserOpd = App\Models\VerifikatorTempat::where('user_id', Auth::user()->id)->count();
+@endphp
+<ul class="sidebar-menu">
+    <li class="header"><strong>MAIN NAVIGATION</strong></li>
+    <li>
+        <a href="{{ route('home') }}">
+            <i class="icon icon-home red-text s-18"></i>
+            <span>Home</span>
+        </a>
+    </li>
+    @can('master-role')
+    <li class="header light"><strong>ROLE</strong></li>
+    <li class="no-b">
+        <a href="{{ route('master-role.permission.index') }}">
+            <i class="icon icon-clipboard-list text-red s-18"></i>
+            <span>Permission</span>
+        </a>
+    </li>
+    <li>
+        <a href="{{ route('master-role.role.index') }}">
+            <i class="icon icon-key3 amber-text s-18"></i>
+            <span>Role</span>
+        </a>
+    </li>
+    @endcan
+    @if (in_array($role, [1,5,6]) && $getUserOpd == 0)
+        @can('master-pegawai')
+        <li class="header light"><strong>PENGGUNA</strong></li>
+        <li class="no-b">
+            <a href="{{ route('pengguna.index') }}">
+                <i class="icon icon-user-o text-success s-18"></i>
+                <span>Pengguna</span>
+            </a>
+        </li>
+        @endcan
+    @endif
+    @if (in_array($role, [1,5,6]) && $getUserOpd == 0)
+        @can('master-data')
+        <li class="header light"><strong>DATA</strong></li>
+        <li class="no-b">
+            <a href="{{ route('waktu.index') }}">
+                <i class="icon icon-timer text-blue s-18"></i>
+                <span>Waktu</span>
+            </a>
+        </li>
+        <li class="no-b">
+            <a href="{{ route('perangkat-daerah.index') }}">
+                <i class="icon icon-placeholder text-yellow s-18"></i>
+                <span>Perangkat Daerah</span>
+            </a>
+        </li>
+        <li class="no-b">
+            <a href="{{ route('indikator.index') }}">
+                <i class="icon icon-bookmark-o text-blue s-18 mr-2"></i>
+                <span>Indikator</span>
+            </a>
+        </li>
+        <li class="no-b">
+            <a href="{{ route('answer.index') }}">
+                <i class="icon icon-question_answer text-purple s-18"></i>
+                <span>Jawaban</span>
+            </a>
+        </li>
+        <li class="no-b">
+            <a href="{{ route('pertanyaan.index') }}">
+                <i class="icon icon-question mr-2 text-green s-18"></i>
+                <span>Pertanyaan</span>
+            </a>
+        </li>
+        <li class="no-b">
+            <a href="{{ route('kuesioner.index') }}">
+                <i class="icon icon-document-text2 text-red s-18"></i>
+                <span>Quesioner</span>
+            </a>
+        </li>
+        @endcan
+        @can('master-pengisian')
+        <li class="header light"><strong>PENGISIAN</strong></li>
+        <li class="no-b">
+            <a href="{{ route('form-quesioner.index') }}">
+                <i class="icon icon-document-text6 text-yellow s-18"></i>
+                <span>Pengisian Quesioner</span>
+            </a>
+        </li>
+        <li class="no-b">
+            <a href="{{ route('hasil.index') }}">
+                <i class="icon icon-document-text6 text-success s-18"></i>
+                <span>Hasil Quesioner</span>
+            </a>
+        </li>
+        <li class="no-b">
+            <a href="{{ route('revisi.index') }}">
+                <i class="icon icon-document-text6 text-danger s-18"></i>
+                <span>Revisi Quesioner</span> <span class="text-danger font-weight-bold ml-3">{{ $totalRevisi }}</span>
+            </a>
+        </li>
+        @endcan
+    @endif
+    @can('master-verifikasi')
+    <li class="header light"><strong>STATUS PENGISIAN</strong></li>
+    <li class="no-b">
+        <a href="{{ route('statusPengisian.data', 3) }}">
+            <i class="icon icon-percent text-warning s-18"></i>
+            <span>Status Pengisian</span>
+        </a>
+    </li>
+    <li class="no-b">
+        <a href="{{ route('verifikasi.index') }}">
+            <i class="icon icon-verified_user text-blue s-18"></i>
+            <span>Verifikasi</span>
+        </a>
+    </li>
+    @endcan
+</ul>
